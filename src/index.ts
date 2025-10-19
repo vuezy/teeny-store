5846094429afb473a31d6dd8b1685c4a34ea9670
vuezy <zoltandididiop0@gmail.com>
2025-10-19 22:48:32 +0700

    fix: expose persistence plugin

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export type { EnqueueFn, TaskQueue } from './queue';
 export * from './effectProcessor';
 export * from './effectService';
 export * from './computationService';
+export * from './persistencePlugin';
+export * from './types/store';
